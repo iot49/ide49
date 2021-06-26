741ca7928595e85cce76350fd923dea9c75f46fa
Bernhard Boser <boser@berkeley.edu>
2021-06-26 20:07:44 +0000

    fix typo in README

--- plugins/esp-idf/start.sh
+++ plugins/esp-idf/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sleep_forever () {
-    while : ; do
+    while true ; do
         # echo "idling ..."
         sleep 600
     done
@@ -13,20 +13,19 @@ if ! grep -q `hostname` /etc/hosts; then
     sudo bash -c 'echo "127.0.0.1" `hostname` >> /etc/hosts'
 fi
 
-# conditionally mount /home/iot
+# conditionally mount iot
 if [ ${SAMBA:=off} == client ]; then
     sudo mkdir -p ${HOME}
     # keep trying
     while true
     do
-        sudo mount -t cifs //${SAMBA_SERVER_IP}/iot-data ${HOME} \
+        sudo mount -t cifs //${SAMBA_IP}/iot-data ${HOME} \
             -ouid=1000,gid=1000,username=iot,password=${SAMBA_PASSWORD},sec=ntlmssp,domain=WORKGROUP \
         && break
         echo "samba mount failed, keep trying ..."
         sleep 10
     done
 fi
-
 
 echo "sleep forever"
 sleep_forever
