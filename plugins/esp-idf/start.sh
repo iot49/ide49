4cf9f23e9077856cc5c8ea66831369f0576e939e
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-07-22 18:35:51 -0700

    create .init_esp-idf.sh

--- plugins/esp-idf/start.sh
+++ plugins/esp-idf/start.sh
@@ -21,5 +21,12 @@ if [ ${SAMBA:=off} == client ]; then
     done
 fi
 
+# create service init file (sourced by %%service)
+init_file=${HOME}/.init_esp-idf.sh
+if [ ! -f $init_file ]; then
+    echo 'echo setting up IDF ...' > $init_file
+    echo '. $IDF_PATH/export.sh > /dev/null' >> $init_file
+fi
+
 echo "sleep forever"
 sleep_forever
