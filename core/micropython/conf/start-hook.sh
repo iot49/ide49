40d18414867cfa25ebc3d66e182915b6f5cd06b0
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-12-03 12:55:00 -0800

    micropython service

---
+++ core/micropython/conf/start-hook.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+# Configure bluetooth
+# https://github.com/balenablocks/bluetooth
+sudo /bin/bash -c "source /usr/local/bin/entry-bluetooth.sh"
--- core/nginx/start.sh
+++ core/nginx/start.sh
@@ -4,18 +4,18 @@
 export DNS_NAME=${DNS_NAME:=iot49}
 
 export IP=$(curl -s -X GET --header "Content-Type:application/json" \
-        "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | \
-     jq -r ".ip_address")
+           "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | \
+           jq -r ".ip_address")
 
 export HOST_NAME=$(curl -s -X GET --header "Content-Type:application/json" \
-        "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | \
-      jq ".network.hostname")
+           "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | \
+           jq ".network.hostname")
 HOST_NAME="${HOST_NAME%\"}"
 HOST_NAME="${HOST_NAME#\"}"
 
 # install default configuration
 if [[ ! -f /etc/nginx/.config_v1 ]]; then
-    cp -r /usr/local/src/nginx /etc
+    cp -r /usr/local/src/nginx /etc/
     cp /etc/nginx/htpasswd /etc/nginx/htpasswd.default
     cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.default
     echo 'nginx config v1 installed' >/etc/nginx/.config_v1
@@ -24,7 +24,8 @@ fi
 # default html content (landing page & favicon)
 if [[ ! -f /home/iot/.html/index.html ]]; then
     mkdir -p /home/iot/.html
-    cp -r /usr/local/src/nginx/html/* /home/iot/.html
+    cp -r /usr/local/src/nginx/html/* /home/iot/.html/
+    chown -R iot:users /home/iot/.html/
 fi
 
 # configuration for self-signed certificate
