607abf9b9bf863e5a302a5c306136a0367cca7b8
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-12-08 10:27:01 -0800

    update default website

--- plugins/rust/start-hook.sh
+++ plugins/rust/start-hook.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
 # install rust via rustup
-# https://rustup.rs/
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -y
+# https://www.rust-lang.org/tools/install
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs >rustup.sh
+chmod a+x rustup.sh
+./rustup.sh -y
+rm rustup.sh
 
 # install jupyter kernel for rust
 # https://github.com/google/evcxr/blob/main/evcxr_jupyter/README.md
 rustup component add rust-src
-sudo apt install jupyter-notebook cmake build-essential
 cargo install evcxr_jupyter
 evcxr_jupyter --install
