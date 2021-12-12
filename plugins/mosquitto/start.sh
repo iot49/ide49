eb8686f6dfe8dd9be6fecfb815605c92fd419630
Bernhard Boser <bernhard.e.boser@gmail.com>
2021-12-11 19:07:14 -0800

    add MOSQUITTO on/off

--- plugins/mosquitto/start.sh
+++ plugins/mosquitto/start.sh
@@ -10,5 +10,14 @@ if [ ! -f /mosquitto/.config_v1 ]; then
     echo 'mosquitto config v1 installed' >/mosquitto/.config_v1
 fi
 
+# customize device environment (e.g. SAMBA_PASSWORD)
+env49rc=/service-config/iot-home/.env49rc
+if [ -f $env49rc ]; then
+    echo sourcing $env49rc ...
+    source $env49rc
+fi
+
 # start the broker
-/usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
+if [ ${MOSQUITTO:=off}  == on ]; then
+    /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf
+fi
