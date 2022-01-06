21868c65e8dee3b59c3e47bcf1fe3100f9cc121f
Bernhard Boser <bernhard.e.boser@gmail.com>
2022-01-05 16:46:47 -0800

    mosquitto: ssl

--- core/micropython/start-hook.sh
+++ core/micropython/start-hook.sh
@@ -11,6 +11,11 @@ if [ -e /dev/gpiomem ]; then
 EOF
 fi
 
+# set $HOST_IP (for convenience)
+export HOST_IP=$(curl -s -X GET --header "Content-Type:application/json" \
+           "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | \
+           jq -r ".ip_address")
+
 # template for customizing device environment
 env49rc=/service-config/iot-home/.env49rc
 if [ ! -f $env49rc ]; then
--- plugins/mosquitto/start.sh
+++ plugins/mosquitto/start.sh
@@ -3,18 +3,48 @@
 mkdir -p /mosquitto/config
 mkdir -p /mosquitto/data
 mkdir -p /mosquitto/log
+mkdir -p /mosquitto/certs
 
-# install default configuration
-if [ ! -f /mosquitto/.config_v1 ]; then
-    cp /usr/local/src/mosquitto.conf /mosquitto/config
-    echo 'mosquitto config v1 installed' >/mosquitto/.config_v1
-fi
-
-# customize device environment (e.g. SAMBA_PASSWORD)
+# customize device environment (e.g. MOSQUITTO)
 env49rc=/service-config/iot-home/.env49rc
 if [ -f $env49rc ]; then
     echo sourcing $env49rc ...
     source $env49rc
+fi
+
+# install default configuration
+if [ ! -f /mosquitto/.config_v1 ]
+then
+    # create certificates
+    IP=$(curl -s -X GET --header "Content-Type:application/json" \
+           "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" | \
+           jq -r ".ip_address")
+    CA_PWD="iot49"
+    SUBJ="/C=US/ST=CA/L=San Francisco/CN=${IP}"
+    cd /mosquitto/certs
+    openssl genrsa -des3 -passout pass:${CA_PWD} -out ca.key 2048
+    openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -passin pass:${CA_PWD} -subj "${SUBJ}/O=IoT49-CA"
+    openssl genrsa -out broker.key 2048
+    openssl req -new -out broker.csr -key broker.key -subj "${SUBJ}/O=IoT49"
+    openssl x509 -req -in broker.csr -passin pass:${CA_PWD} \
+             -CA ca.crt -CAkey ca.key -CAcreateserial -out broker.crt -days 3650
+    
+    echo IP = ${IP}
+    echo SUBJ = ${SUBJ}
+
+    # create acl file
+    echo "topic readwrite public/#" >/mosquitto/config/public.acl
+
+    # create password file for default user (iot49) and password (iot49)
+    mosquitto_passwd -c -b /mosquitto/config/password_file iot49 iot49
+
+    # install configuration file
+    cp /usr/local/src/mosquitto.conf /mosquitto/config
+
+    # do not overwrite user customizations
+    echo "mosquitto config v1 installed" >/mosquitto/.config_v1
+else
+    echo "not updating config v1"
 fi
 
 # start the broker
