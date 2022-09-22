270dadd6dbea10733742cbd6a4dfbf9b94cc7377
Bernhard Boser <boser@berkeley.edu>
2022-09-22 16:58:34 -0700

    esphome UDEV

--- services/esphome/conf/start.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# Mount dynamically plugged devices (/dev). Requires privileged container.
-/bin/bash -c "UDEV=${UDEV:-on}; source /usr/local/bin/balena-entry.sh"
-
-# Start esphome
-dashboard /config
