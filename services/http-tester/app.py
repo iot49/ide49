e62daa329c23f2c1c4fa4ec2e09fe6955774c50a
Bernhard Boser <boser@berkeley.edu>
2022-10-10 08:18:24 -0700

    http-tester

--- services/http-tester/app.py
+++ services/http-tester/app.py
@@ -3,13 +3,23 @@
 import http.server
 import os
 
-port = os.getenv('HTTP_TESTER_PORT', 8000)
+port = int(os.getenv('HTTP_TESTER_PORT', 8000))
 
 class Handler(http.server.SimpleHTTPRequestHandler):
 
     def do_GET(self):
-        print(self.headers)
-        self.super().do_GET()
+        self.send_response(200)
+        self.send_header('Content-type', 'text/text')
+        self.end_headers()
+
+        self.wfile.write(f"Request Line: {self.requestline}\n\n".encode())
+
+        self.wfile.write(f"Client Address: {self.client_address}\n\n".encode())
+
+        self.wfile.write(b"Request Headers\n")
+        self.wfile.write(b"===============\n\n")
+        for k,v in self.headers.items():
+            self.wfile.write(f"{k}: {v}\n".encode())
 
 
 httpd = http.server.HTTPServer(('', port),  Handler)
