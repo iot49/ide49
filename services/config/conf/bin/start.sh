05f8d57839025be434c2cff4a7f6c35af7829d78
Bernhard Boser <boser@berkeley.edu>
2022-10-18 15:40:29 -0700

    rebuild config, add syncthing

--- services/config/conf/bin/start.sh
+++ services/config/conf/bin/start.sh
@@ -3,6 +3,7 @@
 # default configuration
 config=/service-config/config
 chown -R 0:100 ${config}
+chmod g+w ${config}
 cd ${config}
 rsync --ignore-existing -a --chown 0:100 /usr/local/config/ .
 
@@ -10,8 +11,14 @@ rsync --ignore-existing -a --chown 0:100 /usr/local/config/ .
 sed -i '/^HOST_IP=/d' .env
 echo HOST_IP=`ip route | awk '{print $3}' | head -n 1` >> .env
 
+IP=$(curl -X GET --header "Content-Type:application/json" \
+    "$BALENA_SUPERVISOR_ADDRESS/v1/device?apikey=$BALENA_SUPERVISOR_API_KEY" \
+  | jq -r ".ip_address" \
+  | awk '{print $1;}')
+sed -i '/^IP=/d' .env
+echo IP=${IP} >> .env
+
 if ! grep -q "MDNS_DOMAIN=" ".env"; then
-  echo add ide49
   echo MDNS_DOMAIN=ide49 >> .env
 fi
 
@@ -21,19 +28,17 @@ set -a; source .env; set +a
 source /usr/local/bin/configure-bluetooth.sh
 
 # set hostname = MDNS_NAME
-if [ -z ${BALENA_SUPERVISOR_ADDRESS} ]; then
-  HOST_NAME=$(curl -s -X GET --header "Content-Type:application/json" \
-            "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | \
-            jq ".network.hostname")
-  HOST_NAME="${HOST_NAME%\"}"
-  HOST_NAME="${HOST_NAME#\"}"
+HOST_NAME=$(curl -s -X GET --header "Content-Type:application/json" \
+          "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY" | \
+          jq ".network.hostname")
+HOST_NAME="${HOST_NAME%\"}"
+HOST_NAME="${HOST_NAME#\"}"
 
-  if [[ $HOST_NAME != $MDNS_DOMAIN ]]; then
-      echo changing hostname from $HOST_NAME to $MDNS_DOMAIN
-      curl -X PATCH --header "Content-Type:application/json" \
-          --data '{"network": {"hostname": "'"$MDNS_DOMAIN"'" } }' \
-          "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
-  fi
+if [[ $HOST_NAME != $MDNS_DOMAIN ]]; then
+    echo changing hostname from $HOST_NAME to $MDNS_DOMAIN
+    curl -X PATCH --header "Content-Type:application/json" \
+        --data '{"network": {"hostname": "'"$MDNS_DOMAIN"'" } }' \
+        "$BALENA_SUPERVISOR_ADDRESS/v1/device/host-config?apikey=$BALENA_SUPERVISOR_API_KEY"
 fi
 
 sleep infinity
