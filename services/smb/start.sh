683fcf4ea419915f36f3a0f67515b156aa24aac6
Bernhard Boser <boser@berkeley.edu>
2022-10-21 20:22:32 -0700

    x

--- services/smb/start.sh
+++ services/smb/start.sh
@@ -3,8 +3,8 @@
 function setup_user {
     # TODO: check if user exists already to get rid of log message
     #    useradd: user 'iot' already exists
-    SMB_USER=${SAMBA_USERNAME:=iot}
-    SMB_PASS=${SAMBA_PASSWORD:=iot49}
+    SMB_USER=${SAMBA_SERVER_USERNAME:=iot}
+    SMB_PASS=${SAMBA_SERVER_PASSWORD:=iot49}
     useradd --no-create-home -g users -u 1000 $SMB_USER
     usermod -aG sambashare $SMB_USER
     printf "%s\n%s\n" ${SMB_PASS} ${SMB_PASS} | smbpasswd -a -s $SMB_USER
@@ -12,7 +12,7 @@ function setup_user {
 }
 
 function main {
-    if [ "$SAMBA" == server ]
+    if [ ! -z "$SAMBA_SERVER_PASSWORD" ]
     then
         echo "Start samba server"
         setup_user
@@ -24,7 +24,7 @@ function main {
 }
 
 # load environment
-env_file=/service-config/iot-home/.env
+env_file=/service-config/config/.env
 set -a; [[ -f ${env_file} ]] && source ${env_file}; set +a
 
 main $@
